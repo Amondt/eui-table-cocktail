e7bd8ac521201c82492466a43f1cd9c4903ebcde
MONDT Alexandre (DIGIT-EXT) <Alexandre.MONDT@ext.ec.europa.eu>
2022-08-04 17:32:22 +0200

    add selectable displayed rows

--- src/app/features/module2/module2.component.ts
+++ src/app/features/module2/module2.component.ts
@@ -1,30 +1,37 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 import { EuiTableComponent } from '@eui/components/eui-table';
+import { UxLink } from '@eui/base';
 
 @Component({
     // tslint:disable-next-line
     selector: 'module2',
     templateUrl: './module2.component.html',
-    styles: [`
-    .my-highlight {
-        background-color: var(--eui-color-danger-lightester);
-        font-weight: bold;
-        text-decoration: underline;
-    }
-    `],
     encapsulation: ViewEncapsulation.None,
 })
 export class Module2Component implements OnInit {
-    constructor(private http: HttpClient) { }
-
     public isCompact: boolean = false;
 
     public dataSource: any[] = [];
     public filteredData: any[] = [];
 
+    public form: FormGroup;
+    public colsValues: UxLink[] = [
+        new UxLink({ label: 'Title', id: 'title' }),
+        new UxLink({ label: 'First name', id: 'first' }),
+        new UxLink({ label: 'Last name', id: 'last' }),
+        new UxLink({ label: 'Nationality', id: 'nat' }),
+        new UxLink({ label: 'Email', id: 'email' }),
+        new UxLink({ label: 'Phone', id: 'phone' }),
+        new UxLink({ label: 'Date of birth', id: 'dob' }),
+    ];
+    public visibleCols = ['title', 'first', 'last', 'nat', 'email', 'phone', 'dob'];
+
     @ViewChild('euiTable') euiTable: EuiTableComponent;
+
+    constructor(private http: HttpClient, private fb: FormBuilder) { }
 
     ngOnInit() {
         this.http.get("https://randomuser.me/api/?results=50").subscribe((res: any) => {
@@ -33,6 +40,13 @@ export class Module2Component implements OnInit {
             this.dataSource = res.results;
             this.filteredData = this.dataSource;
         })
+
+        this.form = this.fb.group({
+            colsFilter: new FormControl(this.colsValues),
+        });
+        this.form.get('colsFilter').valueChanges.subscribe((values: UxLink[]) => {
+            this.visibleCols = values.map(v => v.id);
+        });
     }
 
     public onFilterChange(event: any) {
